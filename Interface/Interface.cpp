801ee3cbe2cb3c0f3c5609bd7d2b0f5535710690
GeorgiyZhilinskiy <79874368+GeorgiyZhilinskiy@users.noreply.github.com>
2024-05-22 17:05:35 +0300

    Add files via upload

---
+++ Interface/Interface.cpp
@@ -0,0 +1,37 @@
+﻿// Interface.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class Figure
+{
+public:
+    virtual double square() = 0;
+};
+
+class Rectangle :public Figure
+{
+public:
+    double a;
+    double b;
+
+    Rectangle(double a, double b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    double square()
+    {
+        return a * b;
+    }
+};
+
+
+int main()
+{
+    Rectangle rect(2, 5);
+    cout << rect.square()<<endl;
+}
+
